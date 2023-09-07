bf25db364aac753b0d801037f515b5dcb4125dd5
Koj, Pascal Marie <pkoj@iu.edu>
2023-09-06 22:14:40 -0400

    almost there

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -7,12 +7,10 @@ class Calculator {
     private var mainTextView: TextView? = null
     private var display = ""
     private var nums = ArrayList<Float>()
-    private var ops = ArrayList<Float>()
-    //private var currentValue: Double = 0.0
-    //private var previousValue: Double = 0.0
-    private var clr = false
-    private var currentOperator: String? = null
-    private var decimalClicked: Boolean = false
+    private var ops = ArrayList<String>()
+    private var clear = false
+    //private var currentOperator: String? = null
+    //private var decimalClicked: Boolean = false
 
 
 
@@ -27,16 +25,13 @@ class Calculator {
 
     fun clear() {
         display = ""
-        //currentValue = 0.0
-        //previousValue = 0.0
-        //currentOperator = null
         nums.clear()
         ops.clear()
-        decimalClicked  = false
         updateDisplay()
     }
 
-    fun negate() {
+    //not using below methods anymore they were giving trouble with updating display and executing the math correctly
+    /*fun negate() {
         //currentValue = -currentValue
         if (display[0] == '-'){display = display.substring(1)}
         else {display = '-'.plus(display)
@@ -53,66 +48,132 @@ class Calculator {
         }
         updateDisplay()
         return
-    }
+    }*/
 
     /*fun setNumber(value: Double) {
         currentValue = value
     }*/
 
-    fun decimal() {
+    /*fun decimal() {
         if (!decimalClicked) {
             //currentValue += 0.0 // Add decimal point
 
             decimalClicked = true
             updateDisplay()
         }
-    }
+    }*/
 
 
-    fun setOperator(operator: String) {
-        currentOperator = operator
-        previousValue = currentValue
-        currentValue = 0.0
-        decimalClicked = false
-    }
-
-    fun operate(text: String): Boolean {
-        return text in arrayOf("+", "-", "*", "/")
+    fun calculate(operator: Char) {
+        calculate(operator.toString())
     }
 
 
 
-    fun calculate(toString: String): Double {
-        var temp = display.toIntOrNull()
-        when (currentOperator) {
-            //do the operation of each button
-            if (temp == null) {
+    fun calculate(operator: String) { //always update display after each operation this was biggest issue
+        if (operator == "C"){ //clearing display
+            clear()
+            return
+        }
 
-                    } else {
-                        ret
+
+
+        if (operator == "+/-"){ //negating
+            if (display[0] == '-'){
+                display = display.substring(1)
+            }
+            else {display = '-'.plus(display)}
+            updateDisplay()
+            return
+        }
+
+
+
+
+        if (operator == "%"){ //percent aka div by 100
+            display = (display.toInt()/100).toString()
+            updateDisplay()
+            return
+        }
+
+
+        var type = operator.toIntOrNull() //checks if user clicks a number or an operator number will returm int and opertor will return null
+        if (type == null){ //means user pressed an operator button
+
+            //check to see what's stored already
+            var checkDisplay = display.toFloatOrNull()
+
+            if (checkDisplay == null){ //nothing in display
+                display = "invalid" }
+            else { // store in num array to keep track of nums pressed until = sign
+                nums.add(checkDisplay) }
+
+
+            if (operator != "="){ //user clicks on any operator other than equals meaning they are not finished with their math
+                ops.add(operator) }
+
+
+
+            if (nums.size == 2){ //this is where we execute the operation since the user has input 2 numbers and an operation
+                var op : String? = ""
+
+                if (operator == "="){
+                    op = ops.removeLastOrNull()
+                }
+
+                if (operator != null){ //where we actually operate and print the result in display
+                    var executed = Apply(nums.get(0), nums.get(1), operator) //execute
+                    display = executed //change the display to the result
+                    nums.clear() //clear the arrays so you can restart the math from scratch with each formula
+                    ops.clear()
+                    clear = true //clear the display
                 }
             }
         }
-    }
 
-    fun PushButton(buttonText: String){
-        when{
-            buttonText == "C" -> clear()
-            buttonText == "+/-" -> negate()
-            buttonText == "%" -> percent()
-            buttonText == "." -> decimal()
-            operate(buttonText) -> setOperator(buttonText)
-            else -> {
-                if (decimalClicked) {
-                    currentValue = (currentValue.toString() + buttonText).toDouble()
-                } else {
-                    currentValue = (currentValue * 10) + buttonText.toDouble()
-                }
-                updateDisplay()
+
+        else{
+            if (!ops.isEmpty() && clear){
+                display = ""
+                clear = false
             }
+            display = display.plus(operator)
         }
 
+        updateDisplay()
+        println(this.nums)
+        println(this.ops)
+
     }
+
+
+
+
+    fun Apply(num1: Float, num2: Float, operator: String): String {
+        var res = "invalid operator"
+        if (operator == "+") {
+            res = (num1 + num2).toString()
+        }
+        if (operator == "-") {
+            res = (num1 - num2).toString()
+        }
+        if (operator == "*") {
+            res = (num1 * num2).toString()
+        }
+        if (operator == "/") {
+            res = if (num2.toInt() == 0) {
+                "Error"
+            } else {
+                (num1 / num2).toString()
+            }
+        }
+        if (res.endsWith(".0")) {
+            res = res.split(".")[0].toInt().toString()
+        }
+        return res
+    }
+
+
 
     companion object{
         var calc = Calculator()
---
+++ app/src/main/java/com/example/calculator/README.md
@@ -0,0 +1,48 @@
+# Project 1 Calculator
+
+User will be able to use this functioning calculator which can add subtract multiply and divide numbers while also being
+able to add decimals, take percentages, and clear the display on the top of the screen
+
+## Functionality
+
+The following **required** functionality is completed:
+
+* [ ] User sees 
+* [ ] User can ...
+* [ ] ...
+* [ ] ...
+
+The following **extensions** are implemented:
+
+* ...
+* none
+* ...
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='walkthrough.gif' title='Video Walkthrough' width='50%' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Getting familiar with Kotlin while also completing an already seemingly difficult project was very hard. I had
+to get a lot of help from my partner and do not think I would have been able to complete this on my own
+
+## License
+
+    Copyright 2023 Pascal Koj
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
